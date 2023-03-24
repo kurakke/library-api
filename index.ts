d9103c75deee6bae2c01a730fe2aa38883b2f72c
kurakke <kcanggua@gmail.com>
2023-03-25 01:45:55 +0900

    prismaでデータ取ってこれた

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
-const app = express();
-const port = 3001;
+import { PrismaClient } from '@prisma/client';
 
+const app = express();
+const port = 3000;
+const prisma = new PrismaClient();
 app.get('/', (req: Request, res: Response) => {
     res.send('HelloNode');
+})
+
+app.get('/user', async(req: Request, res: Response) => {
+    const users = await prisma.user.findMany();
+    console.log(users);
+    
+    return res.json(users);
 })
 
 app.listen(port, () => console.log('Example App Listening On Port ${port}!'));
