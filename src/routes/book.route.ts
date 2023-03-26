63ffe3b2c976f545a79b5093211e0b93b6a2f71b
kurakke <kcanggua@gmail.com>
2023-03-27 00:06:45 +0900

    routerでget呼び出した

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const bookController = require('../controller/book.controller')
 
+router.get('/book', bookController.get)
 
 module.exports = router
