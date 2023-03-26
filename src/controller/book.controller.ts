d4618cbf123e06db4f92e618ae4d111859431fd4
kurakke <kcanggua@gmail.com>
2023-03-27 00:03:46 +0900

    make get func

---
+++ src/controller/book.controller.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const  get = async () => {
+    const books = await prisma.book.findMany();
+    return books;
+}
