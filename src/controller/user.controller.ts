ab3a17a3e89d32f34b1a8f7856d4be229b5e43cd
kurakke <kcanggua@gmail.com>
2023-04-19 15:09:34 +0900

    トークンの検証のコードが上手くいきません...

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
+import { initializeApp } from "firebase-admin";
+import { getAuth, getIdToken } from "@firebase/auth";
 
+const app = initializeApp();
 const prisma = new PrismaClient();
+const admin = require("firebase-admin");
 
-export const signIn = async (req: Request, res: Response) => {
-    console.log("signIn");
+const auth = getAuth()
+const user = auth.currentUser
+// const idToken = getIdToken(user, true)
+export const logIn = async (req: Request, res: Response) => {
+    const { token, mail } = req.body;
 }
