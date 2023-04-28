9cbb1ce7e4b33c28951bee998e06538cb720917c
kurakke <kcanggua@gmail.com>
2023-04-28 14:28:22 +0900

    tokenの検証書きました

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,14 +2,22 @@ import { Request, Response } from "express";
 import { PrismaClient, User } from "@prisma/client";
 import { initializeApp } from "firebase-admin";
 import { getAuth, getIdToken } from "@firebase/auth";
+import admin from "firebase-admin";
 
-const app = initializeApp();
 const prisma = new PrismaClient();
-const admin = require("firebase-admin");
 
-const auth = getAuth()
-const user = auth.currentUser
-// const idToken = getIdToken(user, true)
 export const logIn = async (req: Request, res: Response) => {
     const { token, mail } = req.body;
+}
+
+export const tokenInspection = (req: Request, res: Response) => {
+    const { token } = req.body;
+    admin.auth().verifyIdToken(token)
+        .then((decodedToken: { uid: string }) => {
+            res.send(decodedToken.uid)
+        })
+        .catch((error: Error) => {
+            console.log('Error verifying token:', error);
+            res.send(error);
+        });
 }
