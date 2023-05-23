363538091304f2e2fb9c03c6f8513ff14a6bf278
kurakke <kcanggua@gmail.com>
2023-05-23 15:51:08 +0900

    signupの処理を作りました

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -8,5 +8,21 @@ export const signIn = (req: Request, res: Response) => {
 }
 
 export const signUp = async (req: Request, res: Response) => {
-    return 'signup'
+    try {
+        const { userId, name, mail, studentNumber } = req.body;
+        const user = await prisma.user.create({
+            data: {
+                id: userId,
+                name: name,
+                mail: mail,
+                role: 'user',
+                studentNumber: studentNumber
+            }
+        })
+        return res.send(user);
+    } catch (e) {
+        if (e instanceof Error) {
+            return res.status(500).send({ error: e.message });
+        }
+    }
 }
