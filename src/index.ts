b72e2635506cb6d031798b47d15ba64fcd34fae8
kurakke <kcanggua@gmail.com>
2023-05-24 02:10:31 +0900

    port番号変えてcookieインストールした

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import userRouter from './routes/user.route';
 import  bookRouter  from './routes/book.route'
 const app = express();
-const port = 3000;
+const port = 8080;
 
  
 app.use('/books', bookRouter)
