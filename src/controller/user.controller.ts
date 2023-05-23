23a55b881e019802ba2d5d7b58bc79cf89e656db
kurakke <kcanggua@gmail.com>
2023-05-23 15:21:27 +0900

    firebaseAuth使わなくなったので削除

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,42 +1,12 @@
 import { Request, Response } from "express";
 import { PrismaClient, User } from "@prisma/client";
-import { initializeApp } from "firebase-admin";
-import { getAuth, getIdToken } from "@firebase/auth";
-import admin from "firebase-admin";
 
 const prisma = new PrismaClient();
 
 export const signIn = (req: Request, res: Response) => {
-    const { token } = req.body;
-    admin.auth().verifyIdToken(token)
-        .then((decodedToken: { uid: string }) => {
-            res.send(decodedToken.uid)
-        })
-        .catch((error: Error) => {
-            console.log(error);
-            res.send(error);
-        });
+    return 'signin'
 }
 
 export const signUp = async (req: Request, res: Response) => {
-    const { token, email, name, studentNumber } = req.body;
-    
-    admin.auth().verifyIdToken(token)
-        .then((decodedToken: { uid: string }) => {
-            const uid = decodedToken.uid;
-            const user = prisma.user.create({
-                data: {
-                    id: uid,
-                    mail: email,
-                    name: name,
-                    role: "normal",
-                    studentNumber: studentNumber
-                }
-            })
-            res.send(uid)
-        })
-        .catch((error: Error) => {
-            console.log(error);
-            res.send(error);
-        })
+    return 'signup'
 }
