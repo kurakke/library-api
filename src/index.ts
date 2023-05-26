3011c8938489c3cd3036e15134a87674bd3a5fab
kurakke <kcanggua@gmail.com>
2023-05-26 20:33:39 +0900

    cors導入

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import userRouter from './routes/user.route';
 import  bookRouter  from './routes/book.route'
 const app = express();
 const port = 8080;
+const cors = require('cors');
 
 app.use('/books', bookRouter)
 app.use('/user', userRouter)
+app.use(cors({
+    origin: 'http://localhost:3000/'
+}))
 app.get('/', (req: Request, res: Response) => {
     res.send('HelloNode');
 });
