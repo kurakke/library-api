436622e425d6fbb545866fd4ded3b3f145d17152
kurakke <kcanggua@gmail.com>
2023-06-22 14:58:31 +0900

    使ってないやつをインポートから消した

--- src/controller/book.controller.ts
+++ src/controller/book.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient, LendRecord } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { Prisma } from '@prisma/client';
 import { normalizeQuery } from '../utils/express'
 
