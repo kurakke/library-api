bc50bc10e10dd0c949939e7471e74cf6f078a18c
kurakke <kcanggua@gmail.com>
2023-06-26 08:08:04 +0900

    corsの設定を本番環境用に追加した

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ const cors = require('cors');
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+app.use(cors({
+    origin: 'https://library-web-develop.vercel.app/'
+}))
 app.use(express.json())
 
 app.use('/books', bookRouter)
